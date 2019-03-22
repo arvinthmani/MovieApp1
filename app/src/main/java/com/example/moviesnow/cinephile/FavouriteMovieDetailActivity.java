3c52f36450187f3ebde5c3c07ab006b99f0b3b7c
Arvinth <arvinth.manivasakan@external.sennheiser.com>
2019-03-22 11:14:03 +0530

    MovieNow
    Movie Now App

---
+++ app/src/main/java/com/example/moviesnow/cinephile/FavouriteMovieDetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.moviesnow.cinephile;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.example.moviesnow.R;
+
+public class FavouriteMovieDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favourite_movie_detail);
+    }
+
+}
